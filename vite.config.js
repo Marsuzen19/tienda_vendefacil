9e6ca7abcd59dd91508010465a7de3c3d81983d6
Marsuzen19 <fernanflo.hptmail@gmail.com>
2026-06-04 11:54:23 -0500

    arregle el error 404 que no me dejaba ver mi pagina web en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Si se está ejecutando en GitHub Actions, usa el nombre del repo, si no, usa la raíz '/'
+  base: process.env.GITHUB_ACTIONS ? '/tienda_vendefacil/' : '/',
 })
